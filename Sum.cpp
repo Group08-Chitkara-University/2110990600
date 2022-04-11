8a9a22c30e7bde5059ed9685ad7dea18c2ce1f10
HarulArora600 <102283122+HarulArora600@users.noreply.github.com>
2022-04-11 12:02:37 +0530

    Sum of numbers

---
+++ Sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cout<<"Input a ";
+    cin>>a;
+    cout<<"Input b ";
+    cin>>b;
+    sum=a+b;
+    cout<<"Sum of a and b= 4"<<sum;
+}
